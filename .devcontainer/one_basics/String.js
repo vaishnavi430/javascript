f8b2d491fbf30d910e3942df076f4baebe93bceb
Vaishnavi Nandu Patole <patolevaishnavi16@gmail.com>
2025-06-16 16:50:22 +0000

    another commit

---
+++ .devcontainer/one_basics/String.js
@@ -0,0 +1,21 @@
+const name = "omkar"
+const age = 34
+
+// console.log(name + age + "Value");
+
+console.log(`Hello my name is ${name} and my age is ${age}`)
+
+const gameName = new String('omkarraj')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('r'))
+
+const newString = gameName.substring(0 , 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-6 , 4)
+console.log(anotherString)
