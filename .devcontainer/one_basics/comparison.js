5de87aa081284887ca2d91939c4d1dfa493e81c7
Vaishnavi Nandu Patole <patolevaishnavi16@gmail.com>
2025-06-16 13:19:57 +0000

    third commit

---
+++ .devcontainer/one_basics/comparison.js
@@ -0,0 +1,20 @@
+// console.log( 2 > 1)
+// console.log( 2 >= 1)
+// console.log( 2 < 1)
+// console.log( 2 != 1)
+// console.log( 2 < 3)
+
+// console.log("02" > 1)
+// console.log("2" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+// do not use this type of comparisons it can create a confusion
+
+console.log("2" === 2)//this is strict checking comparison
---
+++ .devcontainer/one_basics/operations.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*
+console.log(2 + 2)
+console.log(2 * 2)
+console.log(2 / 3)
+console.log(2**3)
+console.log(2 % 3)
+*/
+
+console.log(2 + 2 + "3")
+console.log("2" + 2)
+console.log("2" + "2")
+console.log(2 + "2")
+console.log("2" + 2 + 3)
+console.log((3+4) * 4 % 5)
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
