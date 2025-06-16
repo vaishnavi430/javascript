a4eafc03f1f3a6a3df11c3ac00ebde3411506349
Vaishnavi Nandu Patole <patolevaishnavi16@gmail.com>
2025-06-16 13:46:08 +0000

    fourth commit

---
+++ .devcontainer/one_basics/datasummary.js
@@ -0,0 +1,27 @@
+//Data types
+//1. primitive
+// String, number, boolean, symbol, BigInt , null, undefined
+
+// reference( non primitive)
+//2. non primitive
+//arrays , objects , functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+
+const id = '123'
+const anotherId = '234'
+console.log(id === anotherId)
+
+const BigInt = 4367483999927728n
+console.log(BigInt)
+
+const flowers = ["rose" , "lotus", "mogra"];
+let myObj = {
+    name :"harry",
+    age : 23,
+}
+console.log(flowers)
+console.log(myObj)
